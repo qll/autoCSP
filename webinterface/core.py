345d804ec28b4623f4af175902b32cb69d33a491
qll <qll@iceqll.eu>
2013-08-11 22:26:44 +0200

    Get rid of test URL.

--- webinterface/core.py
+++ webinterface/core.py
@@ -4,8 +4,3 @@ from lib.webinterface import make_response, path
 @path('/')
 def index(req):
     return make_response('layout.html')
-
-
-@path('/test/(\d+)/(\w+)')
-def test(req, num, word):
-    return num + word
