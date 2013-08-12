59cd937450f201a430dbef382613aa684e5db40a
qll <qll@iceqll.eu>
2013-08-12 02:10:38 +0200

    More testcases (style); more CSP-type headers get stripped

--- autocsp/interceptors/csp.py
+++ autocsp/interceptors/csp.py
@@ -3,15 +3,20 @@ from lib.events import subscribe
 
 @subscribe('response')
 def clear_csp_header(request):
-    """ Clears existing CSP headers. """
+    """ Strip existing CSP headers. """
     for field in ('Content-Security-Policy', 'X-WebKit-CSP',
-                  'X-Content-Security-Policy'):
+                  'X-Content-Security-Policy', 'X-WebKit-CSP-Report-Only',
+                  'X-Content-Security-Policy-Report-Only',
+                  'Content-Security-Policy-Report-Only'):
         if field in request.headers:
             del request.headers[field]
 
 
 @subscribe('response')
 def add_report_csp_header(req):
-    """ Adds a report-only CSP header. """
-    req.headers['Content-Security-Policy'] = ["default-src 'self'; ...; "
-                                              "report-uri /_autoCSP/_/report;"]
+    """ Adds a Report-Only CSP header. """
+    policy = ["default-src 'none'; script-src 'none'; style-src 'none'; "
+              "img-src 'none'; connect-src 'none'; font-src 'none'; "
+              "object-src 'none'; media-src 'none'; frame-src 'none'; "
+              "report-uri /_autoCSP/_/report;"]
+    req.headers['Content-Security-Policy-Report-Only'] = policy
