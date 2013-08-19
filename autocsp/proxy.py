d28cefe6bc0f552bb264925d727e2ce6b4ef1a32
qll <qll@iceqll.eu>
2013-08-19 15:48:18 +0200

    Add option to daemonize autoCSP.

--- autocsp/proxy.py
+++ autocsp/proxy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ CSP-injecting reverse HTTP proxy with policy auto-generation. """
+import optparse
 import os
 
 import lib.events
@@ -12,8 +13,15 @@ from libmproxy import proxy
 
 
 def main():
+    options = get_options()
+    if options.daemonize:
+        daemonize()
     # change cwd to the directory of this file
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # create files with 600 and dirs with 700 permissions (63 = 077)
+    os.umask(63)
+    if options.pid != None:
+        write_pid_file(options.pid)
     paths = {k: os.path.expanduser(p) for k, p in settings.PATHS.items()}
     load_interceptors(paths['INTERCEPTORS'], settings.INTERCEPTORS)
     load_views(paths['VIEWS'])
@@ -27,6 +35,47 @@ def main():
     except KeyboardInterrupt:
         controller.shutdown()
         lib.globals.Globals()['db'].close()
+
+
+def get_options():
+    """ Parses command line options and returns a dictionary containing the
+        results. """
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--daemonize', action='store_true', default=None,
+                      help='daemonize autoCSP (fork to background process)')
+    parser.add_option('-p', '--pid', default=None,
+                      help='create file with process ID at given path')
+    return parser.parse_args()[0]
+
+
+def daemonize():
+    """ Forks the program to the background and closes all file descriptors. """
+    fork()
+    os.setsid()
+    fork()
+    # close file descriptors
+    for fd in range(3):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
+    # open /dev/null as filedescriptor
+    os.open(os.devnull, os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+
+def fork():
+    """ Forks and kills parent. """
+    pid = os.fork()
+    if pid > 0:
+        os._exit(0)
+
+
+def write_pid_file(path):
+    """ Writes a file with the process ID to the file system. """
+    with open(path, 'w') as f:
+        f.write(str(os.getpid()))
 
 
 def connect_to_db(path):
