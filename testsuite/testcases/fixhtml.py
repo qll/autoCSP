6fa5bbe0495bd5359fbc78d903d9dd2569fc6d97
qll <qll@iceqll.eu>
2013-08-29 16:02:02 +0200

    Add script to fix html files automagically.

---
+++ testsuite/testcases/fixhtml.py
@@ -0,0 +1,28 @@
+""" Quick helper to add HTML5 DOCTYPE and <title> to every testcase. """
+import os
+import re
+import sys
+
+
+def fixhtml(folder):
+    for dirpath, _, filenames in os.walk(folder):
+        for file in filenames:
+            name, ext = os.path.splitext(file)
+            if ext != '.html':
+                continue
+            path = '%s/%s' % (dirpath, file)
+            title = ' '.join(name.split('-'))
+            shouldbe = '<!DOCTYPE html>\n<title>%s</title>\n' % title
+            with open(path, 'r') as f:
+                content = f.read()
+            if content.startswith(shouldbe):
+                continue
+            content = re.sub('\s*<!DOCTYPE[^>]*>\s*<title>[^<]*</title>\s*', '',
+                             content)
+            with open(path, 'w') as f:
+                f.write(shouldbe + content)
+
+
+if __name__ == '__main__':
+    folder = '.' if len(sys.argv) < 2 else sys.argv[1]
+    fixhtml(folder)
